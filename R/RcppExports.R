65998bfc4513b3fe02f89469ef89031b49fc552e
Pascal Croteau <croteaupascl@gmail.com>
2023-03-31 18:53:00 -0400

    Updated to R 4.2.3

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 unique_id <- function(x, n) {
-    .Call('_matrixset_unique_id', PACKAGE = 'matrixset', x, n)
+    .Call(`_matrixset_unique_id`, x, n)
 }
 
